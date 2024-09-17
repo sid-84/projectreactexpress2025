9e705cdb08255862b998feb9c0a154fbfd7788bb
Sidharth Sankar Patro <sidharth.patro.2010@gmail.com>
2024-09-17 06:01:21 +0000

    express project2

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("HomePage Of The App");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
