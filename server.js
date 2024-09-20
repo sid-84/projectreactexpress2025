2caefaf604e2304661b3cec5d411bb7ea81b9f09
Sidharth Sankar Patro <sidharth.patro.2010@gmail.com>
2024-09-20 05:32:18 +0000

    added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const bookRoutes = require('./routes/bookRouter');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ app.get("/", (req, res) => {
     res.send("HomePage Of The App");
 });
 
+app.use('/api', bookRoutes)
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
